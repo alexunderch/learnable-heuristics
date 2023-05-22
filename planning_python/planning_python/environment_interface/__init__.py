4b81aee6f2f434504831665a3f62675e71c8fe93
alexunderch <chernyavskij.as@phystech.edu>
2023-05-22 11:30:54 +0300

    initial commint

---
+++ planning_python/planning_python/environment_interface/__init__.py
@@ -0,0 +1,4 @@
+# import sys
+# sys.path.insert(0, "../..")
+
+# from planning_python.env_2d import Env2D
---
+++ planning_python/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='planning_python',
+    version='1.0.0',
+    packages=['planning_python'],
+)
